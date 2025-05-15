a37ab19f95bdfe453b57e0c5790241454cd2fd70
bohee111 <bboohhee06@gmail.com>
2025-05-15 14:37:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,6 @@ import pandas as pd
 from dotenv import load_dotenv
 from sklearn.model_selection import train_test_split
 
-from config import ExperimentConfig
 from templates import TEMPLATES
 from experiment import ExperimentRunner
 from code.config import ExperimentConfig
