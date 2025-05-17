7cfde075b63299a6adc0dedb97570c57fb6a5c44
bohee111 <bboohhee06@gmail.com>
2025-05-18 03:38:44 +0900

    Update output_parser.py

--- code/utils/output_parser.py
+++ code/utils/output_parser.py
@@ -1,2 +1,5 @@
+def clean_prefix(text: str) -> str:
+    # '교정:', '잘못:' 등의 말머리를 제거하고 strip
+
 def clean_column(df: pd.DataFrame, column: str = "cor_sentence") -> pd.DataFrame:
     # DataFrame의 지정 열에 대해 clean_prefix 적용
